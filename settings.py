a6ece1e7e8991349828c83cb921d5f2a2bb80d56
Александр Морозов <morozov460336@yandex.ru>
2023-09-02 11:51:29 +0300

    third commit, add class Buttom and Settings

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Класс для управления настройками игры."""
+    def __init__(self):
+        self.screen_width = 1
+        self.screen_height = 1
+        self.bg_color = (230, 230, 230)
+
+        self.game_active = True
--- tetris.py
+++ tetris.py
@@ -1,3 +1,29 @@
 import sys
 import pygame
 
+from settings import Settings
+
+class Tetris:
+    """Класс для управления игрой."""
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption('Tetris')
+    
+
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        if self.settings.game_active:
+            pass
+
+        self._update_screen()
+    
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        
+        pygame.display.flip()
+
