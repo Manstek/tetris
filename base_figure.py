ef19ac365b4c8060575dc3e6a7da4cadffe421c4
Александр Морозов <morozov460336@yandex.ru>
2023-09-02 12:22:27 +0300

    add base_figure.py

---
+++ base_figure.py
@@ -0,0 +1,8 @@
+from Settings import settings
+
+class BaseFigure:
+    """Базовый квадрат из которого состоят все фигуры."""
+    def __init__(self):
+        self.settings = Settings()
+
+        
--- settings.py
+++ settings.py
@@ -1,8 +1,12 @@
 class Settings:
     """Класс для управления настройками игры."""
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 600
+        self.screen_height = 500
         self.bg_color = (230, 230, 230)
 
         self.game_active = True
+
+        self.base_figure_width = 20
+        self.base_figure_height = 20
+        
